0433d77d7f46a1870b23405ebae8d48b13d414a6
Zack Siri <zack@artellectual.com>
2013-10-14 18:32:47 +0700

    added initial code

--- lib/has_attachable.rb
+++ lib/has_attachable.rb
@@ -1,5 +1,58 @@
 require "has_attachable/version"
 
 module HasAttachable
-  # Your code goes here...
+extend ActiveSupport::Concern
+
+  class NoUploaderError < StandardError
+    def message
+      "No Uploader found, please specify an uploader"
+    end
+  end
+
+  included do 
+    after_update -> {
+      AttachableWorker.
+        perform_async('process', 
+                       processing_options) if process_attachable?
+      AttachableWorker.
+        perform_async('remove', 
+                       processing_options) if remove_attachable?
+    }
+    include Attachable::Processing
+  end
+
+  module ClassMethods
+    def has_attachable(*attachable_options)
+      field = attachable_options[0]
+      options = attachable_options[1]
+      options[:type] = :graphic unless options[:type].present?
+      initialize_attachable(field, options)
+    end
+
+    private 
+
+    def initialize_attachable(field, options)
+      class_attribute :attachable_options unless defined? self.attachable_options
+      self.attachable_options ||= {}
+      self.attachable_options[field] = options
+
+      class_attribute :attachable_fields unless defined? self.attachable_fields
+      self.attachable_fields ||= []
+      self.attachable_fields << field
+
+      raise NoUploaderError          unless attachable_options[field][:uploader].present?
+      attachable_field = :attachable unless field.present?
+      
+
+      class_eval do 
+        attr_accessible "async_remove_#{field}".to_sym, "#{field}_name".to_sym
+        attr_accessor "async_remove_#{field}".to_sym
+
+        mount_uploader field, 
+                       attachable_options[field][:uploader], 
+                       mount_on: "#{field}_name"
+
+      end
+    end
+  end
 end
